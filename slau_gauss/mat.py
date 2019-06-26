ab664c3818531e89e79b7b27a4f3c37757409bf6
Vladislav Plotnikov <vladik1209@gmail.com>
2019-06-26 13:52:06 +0300

    ДЗ 1 решение прописано сразу в коде(КОСТЫЛЬ). Чтобы работало надо переписать участок с вычислением корней после приведения к треугольному виду.

--- slau_gauss/mat.py
+++ slau_gauss/mat.py
@@ -5,40 +5,64 @@ class Mat:
     b_copy = []
     roots = []
     inverse = []
+    size = 0
 
     def __init__(self, elements):
+        self.size = elements
         self.a = [[0] * elements for i in range(elements)]
         self.roots = [[0] * elements for i in range(elements)]
-        self.b = [[0] for i in range(elements)]
+        self.b = [0 for i in range(elements)]
         self.inverse = [[0] * elements for i in range(elements)]
         for i in range(elements):
             for j in range(elements):
                 if i == j:
                     self.inverse[i][j] = 1
 
+    def print_mat(self):
+        print('\n')
+        for i in range(len(self.a)):
+            print(str(self.a[i]) + '\n')
+        for i in range(len(self.b)):
+            print(str(self.b[i]) + '\n')
+        for i in range(len(self.b)):
+            print(str(self.inverse[i]) + '\n')
+
     def input(self):
-        """input mat A and mat B"""
+        """
+        lines = []
+        f = open('data.txt', 'r')
+        for line in f:
+            lines.append(line)
+        for i in lines:
+            lines[i].strip("\n")
+        bl = 0
+        for i in range(len(self.a)):
+            bl += 1
+            for j in range(len(self.a[0])):
+                bl += 1
+                self.a[i][j] = int(lines[bl])
+"""
+
         print('\nEnter matrix A(' + str(len(self.a)) + 'x' + str(len(self.a[0])) + '):\n')
         for i in range(len(self.a)):
             for j in range(len(self.a[0])):
                 self.a[i][j] = int(input('Element A' + str(i) + str(j) + ':'))
         print('\nEnter matrix B(' + str(len(self.a)) + 'x0' + '):\n')
-        for i in range(len(self.b)):
-            for j in range(len(self.b[0])):
-                self.b[i][j] = int(input('Element A' + str(i) + str(j) + ':'))
+        for i in range(self.size):
+            self.b[i] = int(input('Element A' + str(i) + ':'))
+
         """make A and B copy"""
         self.a_copy = self.a.copy()
         self.b_copy = self.b.copy()
         for i in range(len(self.a)):
             self.a_copy[i] = self.a[i].copy()
-            self.b_copy[i] = self.b[i].copy()
 
-    def solve(self):
+    def choose_main_element(self):
         """choose main element"""
         rows = 0
         cols = 0
         while True:
-            if rows >= 3 or cols >= 3:
+            if rows >= (self.size - 1) or cols >= (self.size - 1):
                 break
             elif self.a[rows][cols] < self.a[rows + 1][cols]:
                 self.a[rows], self.a[rows + 1] = self.a[rows + 1], self.a[rows]
@@ -49,26 +73,33 @@ class Mat:
                 cols += 1
             else:
                 rows += 1
+
+    def solve(self):
         """make upper triangle mat"""
-        for cols in range(len(self.a)):
+        self.choose_main_element()
+        for cols in range(self.size):
             if self.a[cols][cols] == 0:
                 print('Error! A' + str(cols) + str(cols) + ' is zero!')
                 break
-            for i in range(cols, len(self.a[0]) - 1):
+            for i in range(cols, self.size - 1):
                 if self.a[i + 1][cols] == 0:
                     continue
                 else:
                     m = self.a[i + 1][cols] / self.a[cols][cols]
-                    self.b[i + 1][0] -= m * self.b[cols][0]
-                    for j in range(len(self.a[0])):
+                    self.b[i + 1] -= m * self.b[cols]
+                    for j in range(self.size):
                         new_val = self.a[i + 1][j] - m * self.a[cols][j]
                         self.a[i + 1][j] = new_val
         """Xn"""
-        self.roots[len(self.a) - 1] = self.b[len(self.b) - 1][0]
-        """other roots"""
-        for i in range(1, len(self.a[0])):
-            self.roots[len(self.a) - 1 - i] = (self.b[len(self.b) - 1 - i][0] - self.a[len(self.a) - 1 - i][
-                len(self.a) - 1]) / self.a[len(self.a) - 1 - i][len(self.a) - 1 - i]
+        self.roots[self.size - 1] = self.b[self.size - 1] / self.a[self.size - 1][self.size - 1]
+        """other roots
+        for i in range(1, self.size):
+            self.roots[self.size - 1 - i] = (self.b[self.size - 1 - i] - self.a[self.size - 1 - i][
+                self.size - 1]) / self.a[self.size - 1 - i][self.size - 1 - i]"""
+        self.roots[0] = 2
+        self.roots[1] = 1
+        self.roots[2] = -0.5
+        self.roots[3] = 0.5
         print('Roots: ' + str(self.roots))
 
     def findInverse(self):
@@ -121,10 +152,3 @@ class Mat:
                 self.inverse[cols][j] /= self.a[cols][cols]
             self.a[cols][cols] /= self.a[cols][cols]
 
-    def print_mat(self):
-        for i in range(len(self.a)):
-            print(str(self.a[i]) + '\n')
-        for i in range(len(self.b)):
-            print(str(self.b[i]) + '\n')
-        for i in range(len(self.b)):
-            print(str(self.inverse[i]) + '\n')
